82b2c7472086cdb24b979144ac4c151ca2e99d04
Abdias <120845115+Salvojo@users.noreply.github.com>
2025-06-10 23:02:22 -0500

    Add files via upload

---
+++ codigo.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <conio.h>
+#include <windows.h>
+#include "Figuras.h"
+#include "abdias.h"
+using namespace std;
+void gotoxy(int x,int y)
+{ 
+    HANDLE hcon;
+    hcon = GetStdHandle(STD_OUTPUT_HANDLE);   
+    COORD dwPos;   
+    dwPos.X = x;    
+    dwPos.Y= y;  
+    SetConsoleCursorPosition(hcon,dwPos);
+}
+int main()
+{ system("color A0");
+bool contra;
+int a;    
+ cout<<"Ingrese clave ";
+ contra=valcontra();                                                 
+ if(contra==true)
+    { getch();
+        system("cls"); 
+        menuprincipal();    
+
+
+
+
+
+    }else cout<<"ACCESO BLOQUEADO, INTENTE EN 12 MINUTOS.";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
