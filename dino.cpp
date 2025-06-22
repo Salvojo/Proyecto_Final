d8b9c1e24d49ad9c8e3a1fc209fc87ad42f7d210
Abdias <120845115+Salvojo@users.noreply.github.com>
2025-06-21 21:30:29 -0500

    Update dino.cpp
    terminado

--- dino.cpp
+++ dino.cpp
@@ -10,28 +10,45 @@ void gotoxy(int x,int y)
     dwPos.X = x;    
     dwPos.Y= y;  
     SetConsoleCursorPosition(hcon,dwPos);}
-void parado(){
-cout<<"             ########"<<endl;
+
+void parado(int a){  
+system("cls");   
+gotoxy(10,a);
+cout<<"              ########"<<endl;
+gotoxy(10,a+1);
 cout<<"             ####  ####"<<endl;
+gotoxy(10,a+2);
 cout<<"             ##########"<<endl;
+gotoxy(10,a+3);
 cout<<"             ######"<<endl;
+gotoxy(10,a+4);
 cout<<" #          #########"<<endl;
+gotoxy(10,a+5);
 cout<<" ####    ########"<<endl;
+gotoxy(10,a+6);
 cout<<" #####  #############"<<endl;
+gotoxy(10,a+7);
 cout<<" ################  ##"<<endl;
+gotoxy(10,a+8);
 cout<<"   #############"<<endl;
+gotoxy(10,a+9);
 cout<<"     #####  #####"<<endl;
+gotoxy(10,a+10);
 cout<<"     ##    ##"<<endl;
+gotoxy(10,a+11);
 cout<<"     #     #"<<endl;
-cout<<"     ##    ##"<<endl;}    
+gotoxy(10,a+12);
+cout<<"     ##    ##"<<endl;
+}    
+
 void figura(int c){
     bool control=true;
-for(int a=0;a<1000;a++){
-system("color A0");
+for(int a=0;a>=0;a++){
+system("color 04");
 Sleep(100);
 system("cls");
 gotoxy(10,c);
-cout<<"             ########"<<endl;
+cout<<"              ########"<<endl;
 gotoxy(10,c+1);
 cout<<"             ####  ####"<<endl;
 gotoxy(10,c+2);
@@ -59,7 +76,7 @@ cout<<"     ##     "<<endl;
 Sleep(100);
 system("cls");
 gotoxy(10,c);
-cout<<"             ########"<<endl;
+cout<<"              ########"<<endl;
 gotoxy(10,c+1);
 cout<<"             ####  ####"<<endl;
 gotoxy(10,c+2);
@@ -87,20 +104,17 @@ cout<<"            ##"<<endl;
 if (kbhit()){
     char tecla = getch();
      if (tecla == 72){
-            c=5;
-    }}   
+        parado(5);
+        Sleep(95);
+        c=15;
+                    }
+     //if(tecla == 80)
+       // c=15;
 
-
+}   
 }}
-
-void tecla(){
-    if (kbhit()){
-    char tecla = getch();
-     if (tecla == 72){
-        figura(10) ; }
-     }
-}
 
 int main(){
 figura(15);
 }
+
